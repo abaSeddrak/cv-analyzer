211d478950ad3605e952097b8c38959f40a61c06
abanoub <abanoub.anwar@sprinteye.com>
2026-04-08 11:46:28 +0200

    more tex

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ import re
 import spacy
 
 st.title("Upload Your CV")
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    import os
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 text = ""
 
 
